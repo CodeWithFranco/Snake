e3c97746fd33f2a0c38cdaece11f8563614f8174
CodeWithFranco <120446758+CodeWithFranco@users.noreply.github.com>
2024-10-21 22:12:37 -0700

    Just adding High score

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
         Below statements are attributes
         """
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(x=0, y=270)
@@ -16,12 +17,18 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()  # clear the previous text
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 10, "normal"))
+        self.write(f"Score: {self.score} Hight Score: {self.high_score}", align="center", font=("Arial", 10, "normal"))
 
-    def game_end(self):
-        self.penup()
-        self.goto(x=0, y=0)
-        self.write("Game Over", align="center", font=("Arial", 15, "normal"))
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_score()
+
+    # def game_end(self):
+    #     self.penup()
+    #     self.goto(x=0, y=0)
+    #     self.write("Game Over", align="center", font=("Arial", 15, "normal"))
 
     def increase_score(self):
         self.score += 1
